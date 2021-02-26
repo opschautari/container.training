3f4ae48964710110312dc6b74ab0f19a969bf81f
thapakazi <milans.thapa78@gmail.com>
2021-02-26 09:01:29 +0545

    read - eat dirs and chop chop md files :D

--- slides/seed_dibya.js
+++ slides/seed_dibya.js
@@ -1,13 +1,36 @@
 #!/usr/bin/env node
 
 var fs = require('fs')
+var path = require('path');
 
 const isHeading = item => /#.*/.test(item)
 const isEmptyLine = item => item == ""
 const isSeperator = item => item == "---"
 
 try {
-  var data = fs.readFileSync('./opschautari/pre-requisite.md','utf8')
+  var dirPaths = process.argv.slice(2);
+  for (dirPath of dirPaths ){
+    console.log("Inside dir: ",dirPath )
+
+    fs.readdir(dirPath, function(err, files){
+      filesList = files.filter(function(e){
+        return path.extname(e).toLowerCase() === '.md'
+      });
+
+      filesList.forEach(file => parseContents(path.join(dirPath,file)))
+      console.log(filesList);
+    });
+
+  }
+
+} catch(err){
+  console.log("error:",err);
+}
+
+function parseContents(filePath){
+  
+  console.log("Reading file:",filePath )
+  var data = fs.readFileSync(filePath,'utf8')
   var headings= []
   var content = []
   var section = []
@@ -24,7 +47,5 @@ try {
       }
     }
   )
-  console.log(content)
-} catch(err){
-  console.log("error:",err);
+  //console.log(content)
 }
